d70da2759b5a31c57993347b386b73b82283c198
Malcolm <malcolm.ryan@gmail.com>
2020-05-16 19:35:43 +1000

    Basic movement

---
+++ Mapping/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : MonoBehaviour
+{
+    new private Rigidbody2D rigidbody;
+
+    public float speed = 5;
+    public float sightRadius = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 v;
+        v.x = Input.GetAxis("Horizontal");
+        v.y = Input.GetAxis("Vertical");
+
+        v = v.normalized * Mathf.Max(Mathf.Abs(v.x), Mathf.Abs(v.y));
+
+        rigidbody.velocity = v * speed;        
+    }
+}
