9f928e7cbcd557f0f2c1856c4573678115c1e665
Malcolm <malcolm.ryan@gmail.com>
2020-05-16 20:03:46 +1000

    Raycasting vision

--- Mapping/Assets/Scripts/PlayerSensing.cs
+++ Mapping/Assets/Scripts/PlayerSensing.cs
@@ -9,15 +9,48 @@ public class PlayerSensing : MonoBehaviour
 
     public float sightRadius = 10;
     
-    public int nSightRays = 10;
+    public int nSightRays = 100;
     public LayerMask wallLayer;
+
+    private float[] rays;
+    private bool[] hit;
+
+    void Start() {
+        rays = new float[nSightRays];
+        hit = new bool[nSightRays];
+    }
 
     void Update() {
         for (int i = 0; i < nSightRays; i++) {
-            float angle = i * TAU / nSightRays;
+            float angle = i * 360f / nSightRays;
 
             Vector2 dir = Vector2.right.Rotate(angle);
-            Physics2D.Raycast(transform.position, dir, sightRadius, wallLayer.value); 
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, sightRadius, wallLayer.value); 
+
+            if (hit.collider == null) {
+                this.rays[i] = sightRadius;
+                this.hit[i] = false;
+            }
+            else {
+                this.rays[i] = hit.distance;
+                this.hit[i] = true;
+            }
+
         }        
+    }
+
+
+    void OnDrawGizmos() {
+        if (!Application.isPlaying) {
+            return;
+        }
+
+        for (int i = 0; i < nSightRays; i++) {
+            float angle = i * 360f / nSightRays;
+            Vector3 dir = Vector2.right.Rotate(angle);
+
+            Gizmos.color = (hit[i] ? Color.red : Color.green);
+            Gizmos.DrawLine(transform.position, transform.position + dir * rays[i]);
+        }
     }
 }
